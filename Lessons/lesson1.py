3882a659520ea38bacedb53af1f4c6977c8ff1be
Marcin Król <szeryfmk@gmail.com>
2020-12-18 18:05:48 +0100

    lesson1 commit

---
+++ Lessons/lesson1.py
@@ -0,0 +1,14 @@
+print(5 + 7)
+print(5 - 3)
+print(2 * 3)
+print(10 / 3)
+print(10 % 3)
+
+print("Hello")
+print('World ' * 5)
+
+'#Pobieranie danych'
+
+zmienna1 = "Hello"
+zmienna2 = input("Input name: ")
+print(zmienna1 + " " + zmienna2)
