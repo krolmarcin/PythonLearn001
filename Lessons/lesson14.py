48ed38e5ea40551d0216f89d8bf23b0d9d73f625
Marcin Król <szeryfmk@gmail.com>
2020-12-18 18:25:17 +0100

    lesson14 updated

--- Lessons/lesson14.py
+++ Lessons/lesson14.py
@@ -1,1 +1,12 @@
-plik = open("test.txt", "w")
+plik = open("test.txt", "a")
+if plik.writable():
+    ile = plik.write(input("Wprowadź tekst: ") + "\n")
+    print("Zapisano: ", ile)
+plik.close()
+
+plik = open("test.txt", "r")
+
+if plik.readable():
+    print("Zawartość pliku:")
+    for l in plik:
+        print(l)
