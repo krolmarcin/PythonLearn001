2c227574b885d05e33044c91ccc357dfcadbfcb7
Marcin Król <szeryfmk@gmail.com>
2020-12-19 12:51:41 +0100

    lesson37

---
+++ Lessons/lesson37.py
@@ -0,0 +1,29 @@
+# rozpakowanie krotki
+a, b = (2, 5)
+print(a)
+print(b)
+
+# lista
+print()
+x, y = [7, 14]
+
+print(x)
+print(y)
+
+# odwracanie
+print()
+x = 14
+y = 21
+print("x:", x)
+print("y:", y)
+
+print("zamieniam kolejność x i y:")
+x, y = y, x
+print("x:", x)
+print("y:", y)
+
+# 4
+start, *wszystko, koniec = (1, 2, 3, 4, 5, 6, 7)
+print(start)
+print(wszystko)
+print(koniec)
